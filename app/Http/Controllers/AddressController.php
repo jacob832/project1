c1af3bbdf99e22ae56d235138c285d8ef0c89078
JaafarRezk <ja3fora33202@gmail.com>
2023-06-27 02:12:06 -0500

    Profile

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -9,17 +9,43 @@ use App\Http\Traits\GeneralTrait;
 
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreAddressRequest;
+use Symfony\Component\HttpFoundation\Request;
 //use App\Http\Requests\UpdateAddressRequest;
 
 class AddressController extends Controller
 {
 use GeneralTrait;
-    
+  
+public function insert_address(Request $request)
+{
+    $user = Auth::user();
+    if (!$user) {
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+
+    $this->validate($request, [
+        'street_address' => 'required',
+        'area' => 'required',
+        'city' => 'required'
+    ]);
+
+    $address = new Address([
+        'street_address' => $request->input('street_address'),
+        'area' => $request->input('area'),
+        'city' => $request->input('city')
+    ]);
+
+    $user->address()->save($address);
+
+    return response()->json(['message' => 'Address added successfully'], 200);
+}
+
+/*
 public function insert_address(StoreAddressRequest $request)
 {
     $user = Auth::user();
     if(!$user){
-        return $this->returnError('','Error in insert');
+        return $this->returnError('400','Error in insert');
     }
     $address = new Address([
         'street_address' => $request->input('street_address'),
@@ -29,11 +55,12 @@ public function insert_address(StoreAddressRequest $request)
     $user->address()->save($address);
         return $this->returnSuccessMessage('تمت اضافة العنوان بنجاح');
     }
+*/
    public function get_address()
         {
             $user = Auth::user();
             if (!$user) {
-                return $this->returnError('', 'User not found');
+                return $this->returnError('404', 'User not found');
             } else {
                 $address = $user->address;
                 return $this->returnData('تم جلب العنوان بنجاح',$address);

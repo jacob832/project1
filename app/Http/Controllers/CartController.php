4783de8112c0b7aa11017b98e7ca50d38ee476f6
JaafarRezk <ja3fora33202@gmail.com>
2023-06-29 17:09:33 -0500

    Cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -17,7 +17,6 @@ class CartController extends Controller
     use GeneralTrait;
 
     /**
-     * Display the items in the user's cart.
      *
      * @return \Illuminate\Http\Response
      */
@@ -49,16 +48,13 @@ class CartController extends Controller
         $variation = Variation::findOrFail($request->variation_id);
         $item = $cart->items()->where('variation_id', $variation->id)->first();
 
-        // Check if the quantity is greater than zero and less than or equal to the quantity in the Variation table
         if ($request->quantity > 0 && $request->quantity <= $variation->quantity) {
 
             if ($item) {
-                // If the item already exists in the cart, update the quantity and price
                 $item->quantity += $request->quantity;
                 $item->price = $item->quantity * $variation->price;
                 $item->save();
             } else {
-                // If the item doesn't exist in the cart, create a new item
                 $item = new CartItems([
                     'quantity' => $request->quantity,
                     'price' => $request->quantity * $variation->price,
@@ -70,7 +66,6 @@ class CartController extends Controller
             
             $cartItems = $cart->items;
             $totalPrice = $cartItems->sum('price');
-            // Update the quantity of the product in the Variation table
             $variation->decrement('quantity', $request->quantity);
 
             return $this->returnData('Product added to cart successfully', [
@@ -78,7 +73,6 @@ class CartController extends Controller
                 'totalPrice' => $totalPrice,
             ]);
         } else {
-            // Return error message if the quantity is not valid
             return $this->returnError(
                 'Quantity Error',
                 'Failed to add product to cart. The quantity requested is greater than the available quantity.'
@@ -108,10 +102,8 @@ class CartController extends Controller
             {
                 $item = CartItems::findOrFail($id);
         
-                // Update the quantity of the product in the Variation table
                 $item->variation->increment('quantity', $item->quantity);
         
-                // Delete the item from the cart
                 $item->delete();
         
                 $cart = auth()->user()->cart;
@@ -123,31 +115,6 @@ class CartController extends Controller
                 ]);
             }
 
-            /*public function destroy(Cart $cart, Product $product)
-            {
-                // Check if the product exists in the cart
-                $item = $cart->items()->where('variation_id', $product->default_variation_id)->first();
-                if (!$item) {
-                    return $this->returnError(
-                        'Item not found',
-                        'Failed to remove item from cart. The item does not exist in the cart.'
-                    );
-                }
             
-                // Update the quantity of the product in the Variation table
-                $variation = $item->variation;
-                $variation->increment('quantity', $item->quantity);
-            
-                // Remove the item from the cart
-                $item->delete();
-            
-                $cartItems = $cart->items;
-                $totalPrice = $cartItems->sum('price');
-            
-                return $this->returnData('Product removed from cart successfully', [
-                    'cartItems' => $cartItems,
-                    'totalPrice' => $totalPrice,
-                ]);
-            }*/
 }
 
